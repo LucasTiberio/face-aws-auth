451e955849489faa9c4a10378b0db9fd03bf1101
Lucas Tiberio <ltiberio55@gmail.com>
2022-07-10 16:50:23 -0300

    feat: Clear query params after get custom endpoint

--- src/hooks/useApiEndpointTracking.ts
+++ src/hooks/useApiEndpointTracking.ts
@@ -9,14 +9,21 @@ type ApiEndpointTrackingParams = {
 const useApiEndpointTracking = () => {
     const queryParams = useQueryParams<ApiEndpointTrackingParams>();
     const { endpoint } = queryParams;
+    const { history, location } = window
 
     /**
      * Identify wheter endpoint is in query params
      * Set in SessionStorage
      */
     useEffect(() => {
-        if (endpoint) sessionStorage.setItem('endpoint', endpoint);
-    }, [endpoint, queryParams])
+        if (endpoint) {
+            sessionStorage.setItem('endpoint', endpoint);
+
+            // Clear query params
+            const currentUrl = location.origin + location.pathname
+            history.pushState('', '', currentUrl)
+        }
+    }, [endpoint, history, location.origin, location.pathname, queryParams])
 }
 
 export default useApiEndpointTracking
